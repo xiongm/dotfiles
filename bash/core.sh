ef5785e3974fdd018ed0640795eb45feb5948571
xiongm <xiongm@gmail.com>
2015-11-02 20:42:33 -0500

    Update

--- bash/core.sh
+++ bash/core.sh
@@ -1,55 +1,7 @@
-###COLORS###
-
-BLACK='\e[0;30m'
-BLUE='\e[0;34m'
-GREEN='\e[0;32m'
-CYAN='\e[0;36m'
-RED='\e[0;31m'
-PURPLE='\e[0;35m'
-BROWN='\e[0;33m'
-LIGHTGRAY='\e[0;37m'
-DARKGRAY='\e[1;30m'
-LIGHTBLUE='\e[1;34m'
-LIGHTGREEN='\e[1;32m'
-LIGHTCYAN='\e[1;36m'
-LIGHTRED='\e[1;31m'
-MAGENTA='\e[1;35m'
-YELLOW='\e[1;33m'
-LIGHTYELLOW='\e[0;33m'
-WHITE='\e[1;37m'
-NO_COLOR='\e[0m' # No Color
-
-
 export TERM='xterm-256color'
 export EDITOR=vim
 
 if [ -e ~/.dircolors ]; then
  eval `dircolors ~/.dircolors`
 fi
-
-function parse_git_branch {
-  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
-}
-
-# uncomment for a colored prompt, if the terminal has the capability; turned
-# off by default to not distract the user: the focus in a terminal window
-# should be on the output of commands, not on the prompt
-force_color_prompt=yes
-if [ -n "$force_color_prompt" ]; then
-    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
-	# We have color support; assume it's compliant with Ecma-48
-	# (ISO/IEC-6429). (Lack of such support is extremely rare, and such
-	# a case would tend to support setf rather than setaf.)
-	color_prompt=yes
-    else
-	color_prompt=
-    fi
-fi
-
-if [ "$color_prompt" = yes ]; then
-    PS1="$YELLOW\u@\h$NO_COLOR:\w$RED\$(parse_git_branch)$NO_COLOR\$ "
-else
-    PS1='\u@\h:\w\$ '
-fi
-unset color_prompt force_color_prompt
 
