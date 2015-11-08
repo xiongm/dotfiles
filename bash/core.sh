db4d2d090c4e146e879bee5123a82ff459cb2ef8
xiongm <xiongm@gmail.com>
2015-11-07 20:18:02 -0500

    Update

--- bash/core.sh
+++ bash/core.sh
@@ -1,7 +1,11 @@
 export TERM='xterm-256color'
 export EDITOR=vim
 
-if [ -e ~/.dircolors ]; then
- eval `dircolors ~/.dircolors`
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
 fi
 
