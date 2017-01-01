f75e90676ac1e11feb452b0c846bdeab3a236260
xiongm <xiongm@gmail.com>
2017-01-01 00:21:50 -0500

    Added vim plugin installation step

--- install.sh
+++ install.sh
@@ -11,14 +11,22 @@ function install_dot_file() {
   fi
 }
 
+echo "Installing dot files"
 install_dot_file .vimrc vimrc
 install_dot_file .vim vim
 install_dot_file .tmux.conf tmux.conf
 install_dot_file .tmux tmux
 install_dot_file .bash bash
+echo "Done"
 # emacs configs are not very actively maitnained
 # so not enable it for now
 #install_dot_file .emacs emacs
+
+# install everything except for YouCompleteMe
+# since YCM requires some manual installation
+echo "Installing vim plugins"
+vim +PlugInstall +qall
+echo "Done"
 
 
 
