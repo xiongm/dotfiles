00d9ae73dbe6127a09032f39615a4dc44b454bd3
xiongm <xiongm@gmail.com>
2017-01-02 21:38:19 -0500

    Various improvement

--- build_vim.sh
+++ build_vim.sh
@@ -1,13 +1,17 @@
+read -p "This operation will replace existing vim, type 'y' to continue:" answer
+if [[ $answer != 'y' ]]; then
+ exit 0
+fi
 set -x
-echo "Begin installing vim"
-echo "Downloading vim build dependencies"
-echo "Installing git"
-sudo apt-get install git
+sudo apt-get -y install libncurses5-dev libgnome2-dev libgnomeui-dev \
+    libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
+    libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
+    python3-dev ruby-dev libperl-dev git
+mkdir -p build
+cd build
 git clone https://github.com/vim/vim.git
 # Compile vim:
 cd vim
-echo "Running configuration, preparing to compile vim"
-
 # ubuntu 16.04 forbids building both python2 and python3
 # so you have to choose one.
 # leave the option for future reference
@@ -28,7 +32,7 @@ echo "Running configuration, preparing to compile vim"
 
 echo "Compiling vim"
 make && sudo make install
-cd ..
+cd ../..
 echo "Cleanup..."
-rm -fr vim
+rm -fr build
 echo "Completed vim installation"
