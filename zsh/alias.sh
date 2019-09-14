88f2599aa0f2148f01acd0c9ce842699259b7ab2
xiongm <xiongm@gmail.com>
2019-09-14 19:58:06 +0000

    update

---
+++ zsh/alias.sh
@@ -0,0 +1,19 @@
+alias em='emacs -nw'
+alias v='vim'
+alias sv='sudo vim'
+
+alias ll='ls -alFh'
+alias la='ls -A'
+alias l='ls -CF'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+
+alias mkdir='mkdir -pv'
+
+alias ports='netstat -tulanp'
+
+alias diskfile='df -Tha --total'
+alias diskusage='du -ach | sort -h'
+alias ps='ps auxf'
